b80db8f208851801489f6dd9dea96c6b2a217b13
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-05-13 12:50:42 +0200

    Create Item model

---
+++ app/models/item.rb
@@ -0,0 +1,7 @@
+class Item < ApplicationRecord
+  validates :name, presence: :true
+  validates :bottles_number, presence: :true
+  validates :capacity, presence: :true
+  validates :capacity_unit, default: :cl
+  enums capacity_unit: [:dl, :ml, :cl]
+end
