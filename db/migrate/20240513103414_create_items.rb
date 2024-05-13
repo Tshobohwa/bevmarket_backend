bf541caf96c2dc1d0206d99de799d8281df67262
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-05-13 12:35:32 +0200

    Create items migration

---
+++ db/migrate/20240513103414_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :bottles_number
+      t.integer :capacity
+      t.integer :capacity_unit
+
+      t.timestamps
+    end
+  end
+end
