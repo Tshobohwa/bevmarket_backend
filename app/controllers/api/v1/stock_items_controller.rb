57c71073a6f9c6b378e490eb110f25000002ffb9
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-06-08 16:39:46 +0200

    Update stock item

--- app/controllers/api/v1/stock_items_controller.rb
+++ app/controllers/api/v1/stock_items_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::StockItemsController < ApplicationController
   before_action :find_stock_item, only: [:update, :destroy]
+  before_action :update_mode, only: [:update]
 
   # GET api/v1/stock_items
   def index
@@ -26,10 +27,34 @@ class Api::V1::StockItemsController < ApplicationController
 
   # UPDATE api/v1/stock_items/:id
   def update
-    if @stock_item.update(stock_item_params)
-      render json: {status: "success", data: {stock_item: @stock_item}}
+    case @update_mode
+    when 'is_adding_to_stock'
+      # Add available quantity to new quantity
+      quantity = stock_item_params[:quantity] + @stock_item[:quantity]
+      puts "quantity = #{quantity}"
+
+      # set the last unit buy price to the unit buy price of the request
+      last_unit_buy_price = stock_item_params[:unit_buy_price]
+      puts "last unit buy price = #{last_unit_buy_price}"
+
+      # Calculate the average unit buy price
+      # by getting the average between the unit buy price
+      # and quantity of the request and those of the database
+      average_unit_buy_price = ((stock_item_params[:unit_buy_price] * stock_item_params[:quantity] + @stock_item[:average_unit_buy_price] * @stock_item[:quantity]) / (@stock_item[:quantity] + stock_item_params[:quantity])).round(2)
+
+      if @stock_item.update({quantity: quantity, last_unit_buy_price: last_unit_buy_price, average_unit_buy_price: average_unit_buy_price})
+        render json: {status: "success", data: {stock_item: @stock_item}}
+      else
+        render json: {status: "fail", error: {message: "Couldn't update stock item"}}, status: :unprocessable_entity
+      end
+    when 'is_updating_price'
+      if  @stock_item.update({unit_sale_price: stock_item_params[:unit_sale_price], reduction_sale_price: stock_item_params[:reduction_sale_price]})
+        render json: {status: "success", data: {stock_item: @stock_item}}
+      else
+        render json: {status: "fail", error: {message: "Couldn't update stock item"}}, status: :unprocessable_entity
+      end
     else
-      render json: {status: "fail", error: {message: "Couldn't update stock item"}}, status: :unprocessable_entity
+      render json: {status: "fail", error: {message: "please provide update mode"}}, status: :unprocessable_entity
     end
   end
 
@@ -48,6 +73,11 @@ class Api::V1::StockItemsController < ApplicationController
   end
 
   def stock_item_params
-    params.require(:stock_item).permit(:item_id, :average_unit_buy_price, :reduction_sale_price, :last_unit_buy_price, :unit_sale_price, :quantity)
+    params.require(:stock_item).permit(:item_id, :reduction_sale_price, :unit_buy_price, :unit_sale_price, :quantity)
+  end
+
+  # Select if it is updating the price, or it is adding to stock
+  def update_mode
+    @update_mode = params[:update_mode]
   end
 end
