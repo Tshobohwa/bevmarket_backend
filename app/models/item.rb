c16f48d7cd95ec04650630965f1372ae4c50314c
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-06-27 17:27:13 +0200

    Add dependent destroy constraint

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,5 @@ class Item < ApplicationRecord
   validates :capacity, presence: :true
   # validates :capacity_unit, default: 'cl'
   # enums capacity_unit: [:dl, :ml, :cl]
-  has_many :stock_item, foreign_key: :item_id
+  has_many :stock_item, foreign_key: :item_id, dependent: :destroy
 end
