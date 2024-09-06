de527ad0c9bcc41dceb23ff09984c9adf4e69624
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-09-06 12:12:09 +0200

    Create establishements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_29_064122) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_29_135012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_29_064122) do
     t.boolean "is_partener", default: false, null: false
     t.float "credit", default: 0.0, null: false
     t.index ["phone_number"], name: "index_clients_on_phone_number", unique: true
+  end
+
+  create_table "establishments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "expenses", force: :cascade do |t|
