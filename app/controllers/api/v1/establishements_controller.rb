7c7e79319d20a218b60dd5585d798d7c78de285a
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-09-09 23:54:44 +0200

    Update establishement

--- app/controllers/api/v1/establishements_controller.rb
+++ app/controllers/api/v1/establishements_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::EstablishementsController < ApplicationController
+  before_action :find_establishement, only: [:update]
   def index
     @establishements = Establishment.all
 
@@ -15,9 +16,23 @@ class Api::V1::EstablishementsController < ApplicationController
     end
   end
 
+  def update
+    if @establishement.update(establishement_params)
+      render json: {status: "success", data: {establishement: @establishement }}, status: :success
+    else
+      render json: {status: "fail", error: {message: "Couldn;'t update establishement"}}, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def establishement_params
     params.require(:establishement).permit(:name)
+  end
+
+  def find_establishement
+    @establishement = Establishment.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: {status: "fail", message: {message: "Couldn't find record"}}, status: :not_found
   end
 end
