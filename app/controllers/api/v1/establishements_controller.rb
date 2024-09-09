b89cdfe94e51e207539fc7783682aec3d99f6bd9
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-09-09 23:47:29 +0200

    Add create method

--- app/controllers/api/v1/establishements_controller.rb
+++ app/controllers/api/v1/establishements_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::EstablishementsController < ApplicationController
+  def index
+    @establishements = Establishment.all
+
+    render json: {status: "success", data: {establishements: @establishements}}
+  end
+
+  def create
+    @establishement = Establishment.new(establishement_params)
+
+    if @establishement.save
+      render json: {status: "success", data: {establishement: @establishement }}, status: :created
+    else
+      render json: {status: "fail", error: {message: "Couldn't create establishement"}}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def establishement_params
+    params.require(:establishement).permit(:name)
+  end
 end
