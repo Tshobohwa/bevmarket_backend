206e2c60f3bc7cad260d3309afa04840b113a7a0
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-09-11 06:02:13 +0200

    Create sale point type

--- app/models/sale_point.rb
+++ app/models/sale_point.rb
@@ -2,5 +2,6 @@ class SalePoint < ApplicationRecord
   validates :name, presence: true
   belongs_to :establishment, foreign_key: :establishment_id
   validates :address, presence: true
+  validates :sale_point_type, presence: true
   has_many :sale_point_stock_items, foreign_key: :sale_point_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_213433) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_11_040029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_213433) do
     t.integer "establishment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sale_point_type"
     t.index ["establishment_id"], name: "index_sale_points_on_establishment_id"
   end
 
