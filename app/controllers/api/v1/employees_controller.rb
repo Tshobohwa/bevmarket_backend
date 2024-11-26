856f99ee5801e548f7bb73f40f3fbfef7bf418e2
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-11-26 06:35:44 +0200

    Successfully include warehouse and truck in employee object

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::EmployeesController < ApplicationController
   before_action :find_employee, only: [:show, :update, :destroy]
+
   def index
     if !params[:establishment_id].nil? && !params[:user_id].nil?
       @current_employee = Employee.find_by(establishment_id: params[:establishment_id], user_id: params[:user_id])
 
-      render json: { status: "success", data: { current_employee: @current_employee } }
+      render json: { status: "success", data: { current_employee: @current_employee.as_json(include: { sale_point: { include: [:warehouse, :truck] } }) } }
     else
-      @employees = Employee.includes(:user, :establishment, :sale_point).where(establishment_id: current_user.establishment_id)
+      @employees = Employee.includes(:user, :establishment, sale_point: [:warehouse, :truck]).where(establishment_id: current_user.establishment_id)
 
-      render json: { status: "success", data: { employees: @employees.as_json(include: [:user, :establishment, :sale_point]) }}
+      render json: { status: "success", data: { employees: @employees.as_json(include: [:user, :establishment, { sale_point: { include: [:warehouse, :truck] } }]) } }
     end
   end
 
@@ -18,15 +19,15 @@ class Api::V1::EmployeesController < ApplicationController
     @employee = Employee.new(employee_params)
 
     if @employee.save
-      @user.update!({is_employed: true, current_establishment_id: employee_params[:establishment_id]})
-      render json: {status:"success", data: {employee: @employee.as_json(include: [:establishment, :user])}}, status: :created
+      @user.update!({ is_employed: true, current_establishment_id: employee_params[:establishment_id] })
+      render json: { status: "success", data: { employee: @employee.as_json(include: [:establishment, :user, { sale_point: { include: [:warehouse, :truck] } }]) } }, status: :created
     else
-      render json: {status: "fail", error: {message: "Couldn't create employee"}}, status: :unprocessable_entity
+      render json: { status: "fail", error: { message: "Couldn't create employee" } }, status: :unprocessable_entity
     end
   end
 
   def show
-    render json: { data: { employee: @employee.as_json(include: [:establishment, :user]) } }
+    render json: { data: { employee: @employee.as_json(include: [:establishment, :user, { sale_point: { include: [:warehouse, :truck] } }]) } }
   end
 
   def update
@@ -35,14 +36,14 @@ class Api::V1::EmployeesController < ApplicationController
     if @employee.update(employee_params)
       @user.update(user_params)
 
-      render json: {status: "success", data: @employee.as_json(include: [:user, :sale_point, :establishment])}
+      render json: { status: "success", data: @employee.as_json(include: [:user, :sale_point, :establishment, { sale_point: { include: [:warehouse, :truck] } }]) }
     else
-      render json: {status: "fail", error: {message: "couldn't update employee"}}, status: :unprocessable_entity
+      render json: { status: "fail", error: { message: "couldn't update employee" } }, status: :unprocessable_entity
     end
   end
 
   def destroy
-
+    # Implement destroy logic if required
   end
 
   private
@@ -54,11 +55,7 @@ class Api::V1::EmployeesController < ApplicationController
   def find_employee
     @employee = Employee.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    render json: {status:'fail', error: {message: 'Employee not found'}}, status: :not_found
-  end
-
-  def employee_params
-    params.require(:employee).permit(:establishment_id, :sale_point_id, :role, :user_id)
+    render json: { status: 'fail', error: { message: 'Employee not found' } }, status: :not_found
   end
 
   def user_params
