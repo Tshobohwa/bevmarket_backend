00ff56d63ff1e3c6481dd9a3aa9676663c8ad5f2
Lucien Makutano <54644582+lucienmakutano@users.noreply.github.com>
2025-01-02 20:57:34 +0000

    Deploy for prod test

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ az login
 
 az acr login --name bevmarket
 
-docker build -t bevmarket:v0.2.2 . # v0.2.2 is the version version to use
+docker build -t bevmarket:v0.2.4 . # v0.2.4 is the version version to use
 
-docker tag bevmarket:v0.2.2 bevmarket.azurecr.io/bevmarket:v0.2.2
+docker tag bevmarket:v0.2.4 bevmarket.azurecr.io/bevmarket:v0.2.4
 
-docker push bevmarket.azurecr.io/bevmarket:v0.2.2
+docker push bevmarket.azurecr.io/bevmarket:v0.2.4
