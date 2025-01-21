7b44a4accc57b6965b6df0d4c3bda02137dbcb18
Lucien Makutano <54644582+lucienmakutano@users.noreply.github.com>
2025-01-21 13:11:45 +0000

    Minor release - Patch v0.2.8

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ az login
 
 az acr login --name bevmarket
 
-docker build -t bevmarket:v0.2.8 . # v0.2.8 is the version version to use
+docker build -t bevmarket:v0.2.9 . # v0.2.9 is the version version to use
 
-docker tag bevmarket:v0.2.8 bevmarket.azurecr.io/bevmarket:v0.2.8
+docker tag bevmarket:v0.2.9 bevmarket.azurecr.io/bevmarket:v0.2.9
 
-docker push bevmarket.azurecr.io/bevmarket:v0.2.8
+docker push bevmarket.azurecr.io/bevmarket:v0.2.9
