ba950446d4b3f2619409570072fc06586426e5b8
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-02-02 05:34:28 +0200

    Get Stock movements

--- app/controllers/api/v1/stock_movements_controller.rb
+++ app/controllers/api/v1/stock_movements_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::StockMovementsController < ApplicationController
   def index
-    @stock_movements = StockMovement.includes({stocK_item: {include: :item}, })
+    @stock_movements = StockMovement.includes(:stock_item)
     if valid_date?(params[:from]) && valid_date?(params[:to])
       from_date = Date.parse(params[:from])
       to_date = Date.parse(params[:to])
@@ -8,10 +8,10 @@ class Api::V1::StockMovementsController < ApplicationController
     elsif valid_date?(params[:date])
       current_date = Date.parse(params[:date])
       @stock_movements = @stock_movements.where(created_at: current_date.beginning_of_day..current_date.end_of_day, establishment_id: current_user.current_establishment_id)
-    else
-      render json: {status: "success", data: {stock_movements: []}}
     end
-    render json: {status: "success", data: {stock_movements: {}}}
+    render json: {status: "success", data: {stock_movements: @stock_movements.as_json(include: {
+      stock_item: {include: :item}
+    })}}
   end
 
   private
