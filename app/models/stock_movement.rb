855a9c1f812935e7249818c72652b9bdc55c858e
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-02-03 04:18:59 +0200

    Add unit price to stock movement

--- app/models/stock_movement.rb
+++ app/models/stock_movement.rb
@@ -1,6 +1,7 @@
 class StockMovement < ApplicationRecord
   validates :quantity, presence: true
   validates :movement_type, presence: true
+  validates :unit_price, presence: true
 
   belongs_to :stock_item, foreign_key: :stock_item_id
   belongs_to :establishment, foreign_key: :establishment_id
